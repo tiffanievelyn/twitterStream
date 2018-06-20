4ed8ad0e852f1209a32124a73989382abef5b69e
QSRINT\tlim <tiffanievelynn@gmail.com>
2018-06-20 15:42:13 +1000

    Stream twitter content

--- twitterStream/Program.cs
+++ twitterStream/Program.cs
@@ -4,12 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tweetinvi;
+
 namespace twitterStream
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Auth.SetUserCredentials("dCfWgNmcMiUsMmmZkc0uhkZbq", "xETVYjlj8SEuluKRp85BDN9Twn0xDeMLfwrzzlufrzkUjiwz3R", "862690063624192000-9YQ9UZYGlU1trIVxjkn5HIpljnbKLFi", "qCQEPDRdKRT6gSnOtlArAvXHw8BfVnvuECKfaptKwFzEU");
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack("Wednesday");
+            stream.MatchingTweetReceived += (sender, theTweet) =>
+            {
+                Console.WriteLine($"A tweet containing Wednesday has been found; the tweet is {theTweet.Tweet}");
+            };
+            stream.StartStreamMatchingAllConditions();
         }
     }
 }
